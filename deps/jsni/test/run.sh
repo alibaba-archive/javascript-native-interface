8b6b43f1d847c8a2105b86b61091e5fd976be445
jinyue.jy <jinyue.jy@alibaba-inc.com>
2017-07-14 14:29:13 +0800

    Add binding.gyp and binding-coverage.gyp under common-gyp directory. binding-coverage.gyp is used by gencov.sh to test code coverage.

--- deps/jsni/test/run.sh
+++ deps/jsni/test/run.sh
@@ -1,6 +1,6 @@
 for i in `ls -d *-test`;do
   cd $i
-  cp ../common_gyp/binding.gyp .
+  cp ../common-gyp/binding.gyp .
   node-gyp rebuild 1> t.log 2>~/tmp/$i.log
   rm binding.gyp
   node --expose-gc native.js
